c46153ad9d14302b5b7b4f0f8e64169a9f52ad01
Vu Quan <quan.the.vu@advesa.com>
2021-06-23 16:53:26 +0700

    Section 16 - updated

--- Section_12/generics.ts
+++ Section_12/generics.ts
@@ -1,90 +1,99 @@
 // Nothing to do with generics
 const add = (a: number, b: number): number => {
-  return a + b
-}
+  return a + b;
+};
 
 class HoldNumber {
-  data: number
+  data: number;
 }
 
 class HoldString {
-  data:string
+  data: string;
 }
 
-const holdNumber = new HoldNumber()
-holdNumber.data = 123
+const holdNumber = new HoldNumber();
+holdNumber.data = 123;
 
-const holdString = new HoldString()
-holdString.data = 'abc'
+const holdString = new HoldString();
+holdString.data = "abc";
 
 // With GENERICS
 class HoldAnything<TypeOfData> {
-  data: TypeOfData
+  data: TypeOfData;
 
-  add(a: TypeOfData): TypeOfData{
-    return a
+  add(a: TypeOfData): TypeOfData {
+    return a;
   }
 }
 
-const num = new HoldAnything<number>()
-  num.data = 1234
-const str = new HoldAnything<string>()
-  str.data = 'asdfasdf'
-
-
+const num = new HoldAnything<number>();
+num.data = 1234;
+const str = new HoldAnything<string>();
+str.data = "asdfasdf";
 
 class ArrayOfNumbers {
-  constructor(public collection: number[]) { }
-  
+  constructor(public collection: number[]) {}
+
   get(index: number): number {
-    return this.collection[index]
+    return this.collection[index];
   }
 }
 
 class ArrayOfString {
-  constructor(public collection: string[]) { }
-  
+  constructor(public collection: string[]) {}
+
   get(index: number): string {
-    return this.collection[index]
+    return this.collection[index];
   }
 }
 
 class ArrayOfAnything<T> {
-  constructor(public collection: T[]) { }
-  
+  constructor(public collection: T[]) {}
+
   get(index: number): T {
-    return this.collection[index]
+    return this.collection[index];
   }
 }
 
-new ArrayOfAnything<string>(['a', 'b', 'c'])
+new ArrayOfAnything<string>(["a", "b", "c"]);
 
 // Type inference with generics
-const arr = new ArrayOfAnything(['a', 'b', 'c'])
+const arr = new ArrayOfAnything(["a", "b", "c"]);
 
 // Function generics
 function printAnything<T>(arr: T[]): void {
-  for (let i = 0; i < arr.length; i++){
-    console.log(arr[i])
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
   }
 }
 
-printAnything<string>(['a', 'b', 'c'])
+printAnything<string>(["a", "b", "c"]);
 
 // Generic constraints
 class Houses {
   print(): void {
-    console.log('There is some house')
+    console.log("There is some house");
   }
 }
 interface Printable {
-  print():void
+  print(): void;
 }
 
-function printHouseOrCars<T extends Printable>(arr:T[]):void {
+function printHouseOrCars<T extends Printable>(arr: T[]): void {
   for (let i = 0; i < arr.length; i++) {
-    arr[i].print()
+    arr[i].print();
   }
 }
 
-printHouseOrCars([new Houses(), new Houses()])
+printHouseOrCars([new Houses(), new Houses()]);
+
+type Test = {
+  [key: string]: number;
+};
+
+// With ".ts" file, just use <OptTest> to add Generic to arrow function
+const testC = <OptTest>(prop: OptTest): void => {
+  console.log(prop);
+};
+
+testC<Test>({ age: 20 });
--- Section_16/react-redux-ts/src/components/App.tsx
+++ Section_16/react-redux-ts/src/components/App.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { Todo, fetchTodos, deleteTodo } from '../actions';
-import { StoreState } from '../reducers'
+import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos, deleteTodo } from "../actions";
+import { StoreState } from "../reducers";
 
 interface AppProps {
-  todos: Todo[]
+  todos: Todo[];
   // fetchTodos: typeof fetchTodos    Redux will not know what type of fetchTodos => error
   // deleteTodo: typeof deleteTodo
-  fetchTodos: Function
-  deleteTodo: Function
+  fetchTodos: Function;
+  deleteTodo: Function;
 }
 
 interface AppState {
-  fetching: boolean
+  fetching: boolean;
 }
 
 export class _App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
-    super(props)
+    super(props);
 
-    this.state = {fetching: false}
+    this.state = { fetching: false };
   }
 
   onButtonClick = (): void => {
-    this.props.fetchTodos()
-    this.setState({fetching: true})
-  }
+    this.props.fetchTodos();
+    this.setState({ fetching: true });
+  };
 
   onTodoClick = (id: number): void => {
     this.props.deleteTodo(id);
@@ -33,31 +33,56 @@ export class _App extends React.Component<AppProps, AppState> {
 
   componentDidUpdate(prevProps: AppProps): void {
     if (!prevProps.todos.length && this.props.todos.length) {
-      this.setState({fetching: false})
+      this.setState({ fetching: false });
     }
   }
 
   renderList = (): JSX.Element[] => {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id} onClick={()=>this.onTodoClick(todo.id)}>{todo.title}</div>
-    })
-  }
+      return (
+        <div key={todo.id} onClick={() => this.onTodoClick(todo.id)}>
+          {todo.title}
+        </div>
+      );
+    });
+  };
 
   render() {
-    return <div>
-      <button onClick={this.onButtonClick}>Fetch</button>
-      {this.state.fetching ? 'LOADING' : null}
-      {this.renderList()}
-    </div>
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? "LOADING" : null}
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
-const mapStateToProps = ({todos}: StoreState): {todos:Todo[]} => {
-  return {todos}
-}
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
 
 // const mapDispatchToProps = () => {
 
 // }
 
-export const App = connect(mapStateToProps, {fetchTodos,deleteTodo})(_App)
+type keys = "age" | "name";
+type Test = {
+  [key in keys]: number;
+};
+
+// With ".tsx" file, we need to add Generics to arrow function in form: <OptTest,> / <OptTest extends {}> / <OptTest extends unknown>
+const testC = <OptTest,>(prop: OptTest): void => {
+  console.log(prop);
+};
+
+testC<Test>({ age: 20, name: 60 });
+
+type Obj = Record<string, number>;
+const testRecord: Obj = {
+  age: 40,
+  distance: 60,
+  // name: '100'
+};
+
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
